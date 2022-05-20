52539094d4600ef2a4fee0b231e4243d3ebe4974
Noura Algohary <103903785+NouraAlgohary@users.noreply.github.com>
2022-05-20 16:57:16 +0200

    Create 11498-Division-of-Nlogonia-UVA.cpp

---
+++ 11498-Division-of-Nlogonia-UVA.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int k, n, m, x,y;
+	cin >> k;
+	while (k!=0)
+	{
+		cin >> n >> m;
+		while (k--)
+		{
+			cin >> x >> y;
+
+			if (x == n || y == m)
+				cout << "divisa" << endl;
+			else if (x > n && y > m)
+				cout << "NE" << endl;
+			else if (x > n && y < m)
+				cout << "SE" << endl;
+			else if (x<n && y>m)
+				cout << "NO" << endl;
+			else
+				cout << "SO" << endl;
+		}
+		cin >> k;
+	}
+	return 0;
+}
