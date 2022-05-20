dec0b8c75344cb74c6f49ca1b13ddb959c2e10de
Noura Algohary <103903785+NouraAlgohary@users.noreply.github.com>
2022-05-20 16:55:44 +0200

    Create 10878-Decode-the-tape.CPP

---
+++ 10878-Decode-the-tape.CPP
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<math.h>
+#include<string.h>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	string s;
+    getline(cin, s);
+	getline(cin, s);
+	while (s != "___________")
+	{
+		char out=0;
+                if (s[2] == 'o') out +=64;
+                if (s[3] == 'o') out += 32;
+                if (s[4] == 'o') out +=16;
+                if (s[5] == 'o') out +=8;
+                if (s[7] == 'o') out +=4;
+                if (s[8] == 'o') out +=2;
+                if (s[9] == 'o')  out += 1;
+		 cout << (char)out << endl;
+		 		getline(cin, s);
+	}
+	return 0;
+}
